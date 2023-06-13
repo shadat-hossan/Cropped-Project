1de8af50d0ae811f27c54ad8ff5f51137ccea295
Shadat Hossan <shadathossan3500@gmail.com>
2023-06-14 00:05:36 +0600

    all project modifi and anather

--- script/home/nave.js
+++ script/home/nave.js
@@ -3,6 +3,22 @@ const allHadItem = document.querySelector(".header-item");
 const mobileNavbarBtn = document.querySelector(".mobile-navbar-btn");
 
 mobileNavbarBbtn.addEventListener("click", () => {
-  allHadItem.classList.toggle("h-active");
-  mobileNavbarBtn.classList.toggle("h-active");
+    allHadItem.classList.toggle("h-active");
+    mobileNavbarBtn.classList.toggle("h-active");
 });
+
+const header = document.querySelector(".navBar");
+const mainBar = document.querySelector("main");
+
+window.addEventListener('scroll', function() {
+    header.classList.toggle("sticky", window.scrollY > 150)
+})
+
+const active_page = window.location.pathname;
+const navLinks = document.querySelectorAll(".all-had-item a");
+
+navLinks.forEach(links => {
+    if (links.href.includes(`${active_page}`)) {
+        links.classList.add("active")
+    }
+})
